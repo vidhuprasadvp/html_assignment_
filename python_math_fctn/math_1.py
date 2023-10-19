0ecd077456406220ae1459dc92a3bea2897aaa8b
vidhuprasadvp <147972124+vidhuprasadvp@users.noreply.github.com>
2023-10-19 16:00:42 +0530

    first commit

---
+++ python_math_fctn/math_1.py
@@ -0,0 +1,15 @@
+import math
+#print ceil value of 3.700
+print("ceil value:",math.ceil(3.700))
+#print the floor value of -5.12
+print("floor value:",math.floor(-5.12))
+#print the fab value of 9
+print("fab value:",math.fabs(9))
+#print the factorial of 8
+print("factorial:",math.factorial(8))
+#find the gcd value of 4,12
+print("gcd value:",math.gcd(4,12))
+#print the square root of 4
+print("square root of 4:",math.sqrt(4))
+#print the value of pi
+print("pi value:",math.pi)
